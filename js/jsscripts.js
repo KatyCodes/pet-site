70890c95e0f861b1d92340cc1321702a30862228
Jena SanCartier and Katy Henning <jena@>
2016-08-09 11:37:09 -0700

    add bootstrap, jquery, javascript links, add buttons to invert background colors and text

---
+++ js/jsscripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#button1").click(function() {
+    $("body").addClass("black-background");
+    $("body").addClass("text-color-white");
+
+  });
+  $("#button2").click(function() {
+    $("body").removeClass();
+  });
+});
